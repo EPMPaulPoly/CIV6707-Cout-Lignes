67175e650d6b20c609165b7bbd546a1044cfd0ff
PaulCharbonneau <paul.charbonneau@polymtl.ca>
2024-09-22 00:48:17 -0400

    implementation start -

---
+++ backend/src/config.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  database: {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432'),
+  },
+  server: {
+    port: parseInt(process.env.SERVER_PORT || '5000'),
+  }
+};

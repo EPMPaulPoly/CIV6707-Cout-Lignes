bc54966379d2a4668a003c8c9dde47b00cbbde9c
PaulCharbonneau <paul.charbonneau@polymtl.ca>
2024-09-22 00:02:58 -0400

    Feature Implementation complete: add map and center on villeray

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,32 @@
 // File: frontend/src/App.tsx
 
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
 
-const position: LatLngExpression = [45.5017, -73.5673]; // Montreal coordinates
-
-const MapContent: React.FC = () => {
-  const map = useMap();
-  React.useEffect(() => {
-    map.setView(position, 13);
-  }, [map]);
-
-  return (
-    <>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          A sample marker in Montreal
-        </Popup>
-      </Marker>
-    </>
-  );
-};
-
 const App: React.FC = () => {
+  const position: LatLngExpression = [45.549152,-73.61368]; // Montreal coordinates
+
   return (
     <div className="app">
       <h1>My Map Application</h1>
       <div className="map-container">
-        <MapContainer style={{ height: '90%', width: '100%' }}>
-          <MapContent />
+        <MapContainer 
+          center={position} 
+          zoom={13} 
+          style={{ height: '400px', width: '100%' }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              A sample marker in Montreal
+            </Popup>
+          </Marker>
         </MapContainer>
       </div>
     </div>
---
+++ frontend/src/custom-types.ts
@@ -0,0 +1,13 @@
+import { LatLngExpression } from 'leaflet';
+
+declare module 'react-leaflet' {
+  interface MapContainerProps {
+    center: LatLngExpression;
+    zoom: number;
+  }
+
+  interface TileLayerProps {
+    attribution: string;
+  }
+}
+

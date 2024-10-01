f48c83fc47a412a81ad5732d6f321c0185a2f2e0
PaulCharbonneau <paul.charbonneau@polymtl.ca>
2024-10-01 18:13:26 -0400

    Feature Add: Added line contours to Map and App

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import L from 'leaflet';
 import { LatLngExpression } from 'leaflet';
-import { TransitStop } from './types';
+import { TransitStop, TransitLine, LineStop } from './types';
 
 interface MapProps {
   transitStops: TransitStop[];
+  transitLines: TransitLine[];
+  lineStops: LineStop[];
   position: LatLngExpression;
 }
 
-const Map: React.FC<MapProps> = ({ transitStops, position }) => {
+const Map: React.FC<MapProps> = ({ transitStops, transitLines, lineStops, position }) => {
   const largerIcon = L.icon({
     iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
     iconSize: [30, 45],
@@ -19,6 +21,22 @@ const Map: React.FC<MapProps> = ({ transitStops, position }) => {
     shadowSize: [41, 41],
     shadowAnchor: [13, 41]
   });
+
+  const getLineCoordinates = (lineId: number) => {
+    const stopsForLine = lineStops
+      .filter(ls => ls.line_id === lineId)
+      .sort((a, b) => a.order_of_stop - b.order_of_stop);
+
+    return stopsForLine.map(ls => {
+      const stop = transitStops.find(s => s.id === ls.stop_id);
+      return stop ? [stop.latitude, stop.longitude] as LatLngExpression : null;
+    }).filter((coord): coord is LatLngExpression => coord !== null);
+  };
+
+  const getLineColor = (index: number) => {
+    const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff'];
+    return colors[index % colors.length];
+  };
 
   return (
     <div className="map-container">
@@ -39,6 +57,17 @@ const Map: React.FC<MapProps> = ({ transitStops, position }) => {
           >
             <Popup>{stop.name}</Popup>
           </Marker>
+        ))}
+        {transitLines.map((line, index) => (
+          <Polyline
+            key={line.id}
+            positions={getLineCoordinates(line.id)}
+            color={getLineColor(index)}
+            weight={4}
+            opacity={0.7}
+          >
+            <Popup>{line.name}</Popup>
+          </Polyline>
         ))}
       </MapContainer>
     </div>

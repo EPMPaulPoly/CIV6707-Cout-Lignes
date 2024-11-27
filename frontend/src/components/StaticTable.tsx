9b861df918c1df54d49d03be25668a85424c57df
PaulCharbonneau <paul.charbonneau@polymtl.ca>
2024-11-27 03:06:15 -0500

    cleaned linelength formatting

--- frontend/src/components/StaticTable.tsx
+++ frontend/src/components/StaticTable.tsx
@@ -31,6 +31,8 @@ const StaticTable: React.FC<StaticTableProps> = ({
       return parseInt(value).toLocaleString('en');
     } else if (field === 'totalPropertyValue') {
       return parseInt(value).toLocaleString('en');
+    } else if(field ==='lineLength'){
+      return value !== null ? value.toFixed(1) : '';
     }
     return value;
   };
